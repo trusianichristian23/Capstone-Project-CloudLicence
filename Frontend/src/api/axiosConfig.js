e96438642066793d9a801fe41fa5e73cdd09c38a
trusianichristian23 <trusianichristian23@gmail.com>
2026-07-22 17:41:41 +0200

    Update axiosConfig.js

--- Frontend/src/api/axiosConfig.js
+++ Frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://capstone-project-cloudlicence.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
